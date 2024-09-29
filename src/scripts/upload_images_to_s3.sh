cc44965081fc56182dc6b4128628a16f43b3c25e
Joseph P. <joseph.pena.q@gmail.com>
2024-09-29 19:50:12 -0300

    chore: rename bucket

--- src/scripts/upload_images_to_s3.sh
+++ src/scripts/upload_images_to_s3.sh
@@ -1,8 +1,9 @@
+BUCKETE_NAME="minimarket-dataset"
 # Create a S3 bucket named pd-datasets
-aws s3api create-bucket --bucket products-detector-datasets --region us-east-1
+aws s3api create-bucket --bucket ${BUCKET_NAME} --region us-east-1
 # Create two folders in the bucket named test and train
-aws s3api put-object --bucket products-detector-datasets --key test/
-aws s3api put-object --bucket products-detector-datasets --key train/
+aws s3api put-object --bucket ${BUCKET_NAME} --key test/
+aws s3api put-object --bucket ${BUCKET_NAME} --key train/
 # sync datasets to the bucket
-aws s3 sync datasets/test s3://products-detector-datasets/test
-aws s3 sync datasets/train s3://products-detector-datasets/train
+aws s3 sync datasets/test s3://${BUCKET_NAME}/test
+aws s3 sync datasets/train s3://${BUCKET_NAME}/train
