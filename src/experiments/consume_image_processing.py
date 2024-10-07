cfd7c660150d1c4e231ca4a22f6805b54dba225b
Joseph P. <joseph.pena.q@gmail.com>
2024-10-06 21:30:50 -0300

    fix(image-processing): remove token dependency

--- src/experiments/consume_image_processing.py
+++ src/experiments/consume_image_processing.py
@@ -14,7 +14,7 @@ lambda_client = boto3.client(
     region_name="us-east-1",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+    # aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
 )
 
 # Replace with the name of your Lambda function
